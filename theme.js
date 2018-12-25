53680330db08a2081be3396f40f0ef2171fb5670
Gabriel Wu <qqbbnease1004@126.com>
2018-12-25 15:47:28 +0800

    feat: basic structure

--- theme.js
+++ theme.js
@@ -1,12 +1,59 @@
 import theme from 'mdx-deck/themes'
 
 export default {
+  // extends the default theme
   ...theme,
-
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
+  // add a custom font
+  font: 'Roboto, sans-serif',
+  // custom colors
+  colors: {
+    text: '#433d3c',
+    background: '#ede4cd',
+    link: '#274a78',
+  },
+  // css: {
+  //   'text-decoration': 'unset',
+  // },
+  heading: {
+    'background-color': '#f8b862',
+    'box-shadow': '0 0 16px rgba(0, 0, 0, .2)',
+    'text-shadow': '2px 3px rgba(0, 0, 0, .2)',
+    '&:hover': {
+      color: '#59b9c6',
+    },
+  },
+  h1: {
+    fontSize: '5em',
+    textTransform: 'uppercase',
+    '&:hover': {
+      fontSize: '5.1em',
+    },
+  },
+  h2: {
+    fontSize: '3em',
+    '&:hover': {
+      fontSize: '3.1em',
+    },
+  },
+  ul: {
+    'text-decoration': 'unset',
+    // 'list-style-type': 'none',
+  },
+  li: {
+    // 'background-color': 'cyan',
+    '&:hover': {
+      // 'padding': '100 100 100 100',
+      // 'margin': '100 100 100 100',
+    },
+  },
+  a: {
+    'text-align': 'left',
+    'text-decoration': 'unset',
+    '&:hover': {
+      'color': '#83ccd2',
+    },
+    '&:active': {
+      'color': '#eaf4fc',
+    },
+  },
 }
